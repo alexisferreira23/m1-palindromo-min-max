b62c9362afa4a670c5d34c001d3d832ee3c094ad
AlexisFerreira <alexismatheus2013@gmail.com>
2024-07-01 18:06:15 -0300

    Inserido ponto e virgula nos locais necessários para correção do projeto.

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function isPalindrome(str){
     let alt = /[^A-Za-z0-9]/g;   
     str = str.toLowerCase().replace(alt, '');
-    let divStr = str.length
+    let divStr = str.length;
     for(let i = 0; i < divStr/2; i++){
         if(str[i] !== str[divStr -1 -i]){
-            return false
+            return false;
         }
     }
-    return true
+    return true;
 }
 
 function arrayMaxMin(arr){
   
-    let menorNumero = arr[0]
-    let maiorNumero = arr[0]
+    let menorNumero = arr[0];
+    let maiorNumero = arr[0];
 
     for(let i = 0; i < arr.length; i++){
         if(arr[i] < menorNumero){
-            menorNumero = arr[i]
+            menorNumero = arr[i];
         }
         if(arr[i] > maiorNumero){
-            maiorNumero = arr[i]
+            maiorNumero = arr[i];
             
         }
         
     } 
-    return [menorNumero, maiorNumero]
+    return [menorNumero, maiorNumero];
 }   
